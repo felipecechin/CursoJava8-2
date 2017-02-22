133e1fd5e6ceddc8e65f0b522534fa251dc63f2c
Felipe Cechin Mello <ficechin@hotmail.com>
2017-02-22 00:33:07 -0300

    Unidade 1 concluída com sucesso. Java Doc gerado.

---
+++ src/unidade1/Calculadora.java
@@ -0,0 +1,23 @@
+package unidade1;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void dividir(int n1, int n2) throws ArithmeticException {
+		System.out.println(n1/n2);
+	}
+	
+	public static void main(String[] args) throws DivisaoZeroException {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite n1");
+		int n1 = entrada.nextInt(); 
+		System.out.print("Digite n2");
+		int n2 = entrada.nextInt();
+		try {
+		dividir(n1,n2);
+		} catch (ArithmeticException e) {
+			throw new DivisaoZeroException();
+		}
+	}
+}
---
+++ src/unidade1/Teste.java
@@ -0,0 +1,16 @@
+package unidade1;
+
+public class Teste {
+
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+		int i = 0;
+		String frases[]={"um","dois","três"};
+		while (i<4) {
+			
+			System.out.println(frases[i]);
+			i++;
+		}
+
+	}
+
+}
